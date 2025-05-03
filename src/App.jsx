9162280dabdb9b9ca637fa8f7aca4711cc215eb2
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2025-05-03 16:32:42 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,30 @@ import Todo from './components/Todo.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Product from './components/Product.jsx';
 import ProductDetails from './components/ProductDetails.jsx';
+import { useTheme } from './ThemeContext.jsx';
 
 function App() {
+  const { toggleTheme } = useTheme();
+
   return (
     <BrowserRouter>
-      <nav className="p-4 bg-white text-black flex  items-center">
+      <nav className="p-4 bg-inherit text-inherit flex items-center justify-between">
         <div>
-          <Link to="/product" className="mr-4 hover:text-gray-400">Product</Link>
+          <Link to="/product" className="mr-4 hover:underline">Product</Link>
+          <Link to="/" className="mr-4 hover:underline">Home</Link>
         </div>
-        <Link to="/">
-          <button 
-            className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition duration-300"
-          >
-            Home
-          </button>
-        </Link>
+        <button 
+          onClick={toggleTheme}
+          className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition duration-300"
+        >
+          Change Theme
+        </button>
       </nav>
       
       <Routes>
         <Route path="/" element={<Todo />} />
         <Route path="/product" element={<Product />} />
         <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/" element={<Todo />} />
       </Routes>
     </BrowserRouter>
   );
