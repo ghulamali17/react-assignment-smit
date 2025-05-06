d0322b0f279a43c9f5594df63de05967aa552aff
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2025-05-06 13:37:42 +0500

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useTheme } from "../ThemeContext";
 
 function Loading() {
+  const { theme } = useTheme();
+  const isDark = theme === "dark";
+
   return (
-    <div className="bg-white text-white transition-all duration-300 ease-in-out h-screen flex justify-center items-center">
+    <div
+      className={`transition-all duration-300 ease-in-out h-screen flex justify-center items-center ${
+        isDark ? "bg-gray-800" : "bg-white"
+      }`}
+    >
       <div role="status">
         <svg
           aria-hidden="true"
-          className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="w-8 h-8 text-gray-200 animate-spin fill-blue-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
